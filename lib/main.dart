d7070c40ab7ba133b713d699cdbf49591a171405
Donia <doniaamr229@gmail.com>
2024-04-23 13:46:40 +0200

    initialize all settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
-
 import 'package:flutter/material.dart';
-import 'package:graduation_project_fitting_app/core/config/application_theme_manager.dart';
 import 'package:graduation_project_fitting_app/features/categorise/pages/jewelry.dart';
 import 'package:graduation_project_fitting_app/features/categorise/pages/men_category.dart';
 import 'package:graduation_project_fitting_app/features/categorise/pages/shoes_category.dart';
-import 'package:graduation_project_fitting_app/features/categorise/pages/women_category.dart';
-import 'package:graduation_project_fitting_app/features/login/pages/login_view.dart';
 import 'package:graduation_project_fitting_app/features/product/pages/product_view.dart';
-import 'package:graduation_project_fitting_app/features/register/pages/register_view.dart';
-import 'package:graduation_project_fitting_app/features/splash/pages/splash_view.dart';
-import 'package:graduation_project_fitting_app/layout.dart';
+import 'package:graduation_project_fitting_app/features/settings/pages/about_view.dart';
+import 'package:graduation_project_fitting_app/features/settings/pages/edit_profile_screen.dart';
+import 'package:graduation_project_fitting_app/features/settings/pages/help_support_sctreen.dart';
+import 'package:graduation_project_fitting_app/features/settings/pages/rebport_problem_view.dart';
 import 'package:graduation_project_fitting_app/settings_provider.dart';
 import 'package:provider/provider.dart';
+
+import 'core/config/application_theme_manager.dart';
+import 'features/categorise/pages/women_category.dart';
+import 'features/login/pages/login_view.dart';
+import 'features/register/pages/register_view.dart';
+import 'features/splash/pages/splash_view.dart';
+import 'layout.dart';
 
 void main() {
   runApp(
@@ -45,6 +49,10 @@ class MyApp extends StatelessWidget {
         WomenCategory.routeName: (context) =>  const WomenCategory(),
         ShoesCategory.routeName: (context) =>  const ShoesCategory(),
         JewelryCategory.routeName: (context) =>  const JewelryCategory(),
+        EditProfileScreen.routeName: (context) =>  EditProfileScreen(),
+        HelpSupportScreen.routeName: (context) =>  const HelpSupportScreen(),
+        AboutView.routeName: (context) =>  const AboutView(),
+        ReportProblemView.routeName: (context) =>  const ReportProblemView(),
 
       },
     );
